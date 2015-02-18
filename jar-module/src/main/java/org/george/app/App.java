0f24a62fd7db556953f9e7d58eb36593905bef77
George <george@george-gorelishvili.(none)>
2015-02-18 05:35:30 +0400

    initial commit

---
+++ jar-module/src/main/java/org/george/app/App.java
@@ -0,0 +1,14 @@
+package org.george.app;
+
+public class App {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
